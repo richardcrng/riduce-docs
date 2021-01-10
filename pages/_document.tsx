a5bf101f531dcfc8053d07e661e5bc85be20a7b4
richardcrng@gmail.com <richardcrng@gmail.com>
2021-01-10 21:24:13 +0000

    Add 👻 favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,16 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
-import { CssBaseline } from '@geist-ui/react'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import { CssBaseline } from "@geist-ui/react";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    const styles = CssBaseline.flush()
+    const initialProps = await Document.getInitialProps(ctx);
+    const styles = CssBaseline.flush();
 
     return {
       ...initialProps,
@@ -14,12 +20,17 @@ class MyDocument extends Document {
           {styles}
         </>
       ),
-    }
+    };
   }
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link
+            rel="icon"
+            href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👻</text></svg>"
+          ></link>
+        </Head>
         <body>
           {/* <script
             dangerouslySetInnerHTML={{
@@ -53,7 +64,7 @@ class MyDocument extends Document {
           /> */}
         </body>
       </Html>
-    )
+    );
   }
 }
-export default MyDocument
+export default MyDocument;
